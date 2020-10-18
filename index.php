25f293513c0e44c47219aeda855ca91e0f516f44
Thyago Henrique Pacher <73073030+thyago-henrique-pacher@users.noreply.github.com>
2020-10-18 16:38:20 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+	
+	</head>
+	<body>
+		<form action="cep.php" method="get" name="">
+			<p>
+				<label>
+					CEP:
+				</label>
+				<input type="text" name="cep"/>
+			</p>
+			<p>
+				<input type="submit" name="submit" value="Pesquisar"/>
+			</p>
+		</form>
+	</body>
+</html>
